9c2c161d4397bb86ee5788dabfa7fae197bd516c
AndieNOakley <dawl_911@hotmail.com>
2025-08-20 12:24:56 -0600

    Update database.py

--- server/database.py
+++ server/database.py
@@ -1,20 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from .models import Base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./spark.db"
+DATABASE_URL = "sqlite:///./app.db"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
 )
-SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
-def init_db():
-    Base.metadata.create_all(bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
