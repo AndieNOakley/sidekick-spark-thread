8344d62017f5241e0a471de8a374e0b36ac16b0a
AndieNOakley <dawl_911@hotmail.com>
2025-08-20 12:43:06 -0600

    Create database.py

---
+++ server/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from .models import Base  # <- uses your Device/Message models
+
+DATABASE_URL = "sqlite:///./spark.db"
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+# thread-safe session factory
+SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+def init_db():
+    # create tables if they don't exist yet
+    Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
